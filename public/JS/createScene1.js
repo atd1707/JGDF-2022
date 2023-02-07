cdad20442493d05d603dd5585b044b4afadeb018
andy goldie <b00245410@studentmail.uws.ac.uk>
2023-02-07 13:25:04 +0000

    restored
    working 1&2

--- public/JS/createScene1.js
+++ public/JS/createScene1.js
@@ -1,100 +1,42 @@
-function createBox(scene, x, y) {
-  let box = BABYLON.MeshBuilder.CreateBox("box", scene);
-  box.position.x = x;
-  box.position.y = y;
-  return box;
-}
 
-function createLight(scene) {
-  const light = new BABYLON.HemisphericLight(
-    "light",
-    new BABYLON.Vector3(0, 1, 0),
-    scene
-  );
-  light.intensity = 0.7;
-  return light;
-}
 
-function createSphere(scene) {
-  let sphere = BABYLON.MeshBuilder.CreateSphere(
-    "sphere",
-    { diameter: 2, segments: 32 },
-    scene
-  );
-  sphere.position.y = 1;
-  return sphere;
-}
 
-function createGround(scene) {
-  let ground = BABYLON.MeshBuilder.CreateGround(
-    "ground",
-    { width: 6, height: 6 },
-    scene
-  );
-  return ground;
-}
 
-function createArcRotateCamera(scene) {
+function createArcRotateCamera(scene){
   let camAlpha = -Math.PI / 2,
-    camBeta = Math.PI / 2.5,
-    camDist = 10,
-    camTarget = new BABYLON.Vector3(0, 0, 0);
-  let camera = new BABYLON.ArcRotateCamera(
-    "camera1",
-    camAlpha,
-    camBeta,
-    camDist,
-    camTarget,
-    scene
-  );
+  camBeta  =  Math.PI / 2.5,
+  camDist  =  10,
+  camTarget = new BABYLON.Vector3(0, 0, 0); 
+  let camera = new BABYLON.ArcRotateCamera("camera1", camAlpha, camBeta, camDist, camTarget, scene);
   camera.attachControl(true);
   return camera;
 }
 
-function createHouse(scene) {
-  /**** Materials *****/
-  //color
-  const groundMat = new BABYLON.StandardMaterial("groundMat");
-  groundMat.diffuseColor = new BABYLON.Color3(0, 1, 0);
+function createLight(scene){
+  const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0),scene);
+  light.intensity = 0.7;
+  return light;
+}
 
-  //texture
-  const roofMat = new BABYLON.StandardMaterial("roofMat");
-  roofMat.diffuseTexture = new BABYLON.Texture(
-    "/public/assets/images/roof.png"
-  );
-  const boxMat = new BABYLON.StandardMaterial("boxMat");
-  boxMat.diffuseTexture = new BABYLON.Texture(
-    "/public/assets/images/floor.png"
-  );
-
-  /**** World Objects *****/
-  const box = BABYLON.MeshBuilder.CreateBox("box", {});
-  box.material = boxMat;
-  box.position.y = 0.5;
-  const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {
-    diameter: 1.3,
-    height: 1.2,
-    tessellation: 3,
-  });
-  roof.material = roofMat;
-  roof.scaling.x = 0.75;
-  roof.rotation.z = Math.PI / 2;
-  roof.position.y = 1.22;
- 
+function createBox(scene){
+  let box = BABYLON.MeshBuilder.CreateBox("box", scene);
+  box.position.y = 1;
   return box;
 }
 
 export default function createStartScene(engine) {
-  let that = {};
-  let scene = (that.scene = new BABYLON.Scene(engine));
-  //scene.debugLayer.show();
+let that = {};
+let scene = that.scene = new BABYLON.Scene(engine);
+  //scene.clearColor = new BABYLON.Color3.Black;
+//const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
+  //camera.attachControl(true);
+//const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
+  //light.intensity = 0.7;
 
-  let box = (that.box = createBox(scene, 0.5, 0.5));
-  let box2 = (that.box2 = createBox(scene, 0.5, 1.5));
-  let house = (that.house = createHouse(scene));
-  let light = (that.light = createLight(scene));
-  let sphere = (that.sphere = createSphere(scene));
-  let ground = (that.ground = createGround(scene));
-  let camera = (that.camera = createArcRotateCamera(scene));
-  return that;
-}
+that.scene.clearColor = new BABYLON.Color3.Blue;
+that.box = createBox(scene);
+that.camera = createArcRotateCamera(scene);
+
+
+return that;
+};
--- public/JS/createScene2.js
+++ public/JS/createScene2.js
@@ -1,72 +1,21 @@
-var skyMaterial = new BABYLON.SkyMaterial("skyMaterial", scene);
-skyMaterial.backFaceCulling = false;
-
-skyMaterial.turbidity = 1; // Represents the amount (scattering) of haze as opposed to molecules in atmospher
-
-skyMaterial.luminance = 1; // Controls the overall luminance of sky in interval ]0, 1,190[
-
-// Control the planet's orientation over the sun
-skyMaterial.inclination = 0.5; // The solar inclination, related to the solar azimuth in interval [0, 1]
-skyMaterial.azimuth = 0.25; // The solar azimuth in interval [0, 1]
-
-
-
-var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
-skybox.material = skyMaterial;
-
-// Manually set the sun position
-skyMaterial.useSunPosition = true; // Do not set sun position from azimuth and inclination
-skyMaterial.sunPosition = new BABYLON.Vector3(0, 100, 0);
-
-skyMaterial.rayleigh = 2; // Represents the sky appearance (globally)
-
-// Set the horizon elevation relative to the camera position
-skyMaterial.cameraOffset.y = scene.activeCamera.globalPosition.y;
-
-
-function createBox(scene){
-    let box = BABYLON.MeshBuilder.CreateBox("box", scene);
-    box.position.y = 3;
-    box.position.x = 0.5;
-    return box;
-}
-    
-function createLight(scene){
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0),scene);
-    light.intensity = 0.7;
-    return light;
-}
-   
-function createSphere(scene){
-    let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-    sphere.position.y = 1;
-    return sphere;
-}
-   
-function createGround(scene){
-    let ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-    return ground;
-}
-
-function createArcRotateCamera(scene){
-    let camAlpha = -Math.PI / 2,
-    camBeta  =  Math.PI / 2.5,
-    camDist  =  10,
-    camTarget = new BABYLON.Vector3(0, 0, 0); 
-    let camera = new BABYLON.ArcRotateCamera("camera1", camAlpha, camBeta, camDist, camTarget, scene);
-    camera.attachControl(true);
-    return camera;
-}
-
 export default function createStartScene(engine) {
-    let that = {};
-    let scene = that.scene = new BABYLON.Scene(engine);
-    //scene.debugLayer.show();
-
-    let box = that.starbox = createBox(scene);
-    let light = that.light = createLight(scene);
-    let sphere = that.sphere = createSphere(scene);
-    let ground = that.ground = createGround(scene);
-    let camera = that.camera = createArcRotateCamera(scene);
+	let that = {};
+	let scene = (that.scene = new BABYLON.Scene(engine));
+    scene.clearColor = new BABYLON.Color3.Black;
+	const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
+    camera.attachControl(true);
+	const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
+    light.intensity = 0.7;
+	
+	// Skybox
+	var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+	var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+	skyboxMaterial.backFaceCulling = false;
+	skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/images/skybox", scene);
+	skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+	skyboxMaterial.disableLighting = true;
+	skybox.material = skyboxMaterial;			
+	    
     return that;
+
 }
