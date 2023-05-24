72877e4c66a219ba330330b10b096002a288a3c8
andy goldie <65758993+atd1707@users.noreply.github.com>
2023-05-24 14:18:06 +0100

    Update createScenes.js
    230

--- public/JS/createScenes.js
+++ public/JS/createScenes.js
@@ -1,8 +1,8 @@
 import createScene1  from "./createScene1.js";
-import createScene2  from "./createScene2.js";
-import createScene3  from "./createScene3.js";
-import createScene4  from "./createScene4.js";
-import createScene5  from "./createScene5.js";
+//import createScene2  from "./createScene2.js";
+//import createScene3  from "./createScene3.js";
+//import createScene4  from "./createScene4.js";
+//import createScene5  from "./createScene5.js";
 
 const CanvasName = "renderCanvas";
 
@@ -18,11 +18,11 @@ export let scenes = [];
 export let engine = new BABYLON.Engine(canvas, true, null, true);
 
 scenes[0] = createScene1(engine);
-scenes[1] = createScene2(engine);
-scenes[2] = createScene3(engine);
-scenes[3] = createScene4(engine);
-scenes[4] = createScene5(engine);
-scene = scenes[0].scene;
+//cenes[1] = createScene2(engine);
+//scenes[2] = createScene3(engine);
+//scenes[3] = createScene4(engine);
+//scenes[4] = createScene5(engine);
+//scene = scenes[0].scene;
 
 export function setSceneIndex(i){
     scene = scenes[i].scene;
