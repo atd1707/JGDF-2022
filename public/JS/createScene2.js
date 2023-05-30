399e669ddb7edc984a444b7a055b5744313a18f2
andy goldie <b00245410@studentmail.uws.ac.uk>
2023-05-30 15:44:16 +0100

    1544

--- public/JS/createScene2.js
+++ public/JS/createScene2.js
@@ -1,25 +1,4 @@
-/*
-export default function createStartScene(engine) {
-	let that = {};
-	let scene = (that.scene = new BABYLON.Scene(engine));
-    scene.clearColor = new BABYLON.Color3.Black;
-	const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
-    camera.attachControl(true);
-	const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
-    light.intensity = 0.7;
-	
-	// Skybox
-	var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
-	var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-	skyboxMaterial.backFaceCulling = false;
-	skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/images/skybox", scene);
-	skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-	skyboxMaterial.disableLighting = true;
-	skybox.material = skyboxMaterial;			
-	    
-    return that;
-}   
- */   
+
 
 
 
@@ -40,8 +19,7 @@ const xSlide = new BABYLON.Animation(
 const buildHouse = (scene) => {
 const roof = buildRoof();
 const box = buildBox();
-//const box = buildbox2();
-//const roof = buildRoof2();
+
 return BABYLON.Mesh.MergeMeshes([box, roof], true, false, null, false, true);
 }
 
@@ -74,46 +52,15 @@ return roof;
 
 const buildBox = (scene) => {
 const boxMat = new BABYLON.StandardMaterial("boxMat");
-boxMat.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/floor.png");
+boxMat.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/cubehouse.png");
 
 const box = BABYLON.MeshBuilder.CreateBox("box", {});
 box.material = boxMat;
 box.position.y = 0.5;
-//box.scaling.x = 5;
-//box.scaling = new BABYLON.Vector3(2, 1.5, 2);
-//box.rotation.y = BABYLON.Tools.ToRadians(20)
  return box;
 }
 
-//const buildRoof = (scene) => {
-  //  const roofMat = new BABYLON.StandardMaterial("roofMat");
-    //roofMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/roof.jpg");
-    
-    //const roof= BABYLON.MeshBuilder.CreateCylinder2("roof", {diameter: 1.3, height: 1.2, tessellation: 3});
-    //roof.material = roofMat;
-    //roof.position.y = 1.5;
-    //roof.position.x = 0;
-    //roof.position.z = 1.5; 
-    //roof.scaling.x = 0.75;
-    //roof.rotation.z = Math.PI / 2;
-    //roof.position.y = 1.22;
-    
-    //return roof;
-    //}
-    
-    //const buildB0x = (scene) => {
-    //const boxMat = new BABYLON.StandardMaterial("boxMat");
-    //boxMat.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/floor.png");
-    
-    //const box = BABYLON.MeshBuilder.CreateBox2("box", {});
-    //box.material = boxMat;
-    //box.position.y = 1.5;
-    //box.scaling.x = 5;
-    //box.scaling = new BABYLON.Vector3(2, 1.5, 2);
-    //box.rotation.y = BABYLON.Tools.ToRadians(20)
-     //return box;
-    //}
-export default function createStartScene(engine) {
+/export default function createStartScene(engine) {
     let that = {};
     let scene = (that.scene = new BABYLON.Scene(engine));
     scene.clearColor = new BABYLON.Color3.Black;
@@ -142,99 +89,6 @@ export default function createStartScene(engine) {
     skybox.material = skyboxMaterial;           
         
     return that;
+ 
 
-} 
-
-/*
-const showAxis = (scene) => {
-// show axis
-        var showAxis = function (size) {
-        var makeTextPlane = function (text, color, size) {
-        var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
-        dynamicTexture.hasAlpha = true;
-        dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
-        var plane = new BABYLON.Mesh.CreatePlane("TextPlane", size, scene, true);
-        plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
-        plane.material.backFaceCulling = false;
-        plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
-        plane.material.diffuseTexture = dynamicTexture;
-        return plane;
-    };
-
-    var axisX = BABYLON.Mesh.CreateLines("axisX", [
-        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
-        new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
-    ], scene);
-    axisX.color = new BABYLON.Color3(1, 0, 0);
-    var xChar = makeTextPlane("X", "red", size / 10);
-    xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
-    var axisY = BABYLON.Mesh.CreateLines("axisY", [
-        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
-        new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
-    ], scene);
-    axisY.color = new BABYLON.Color3(0, 1, 0);
-    var yChar = makeTextPlane("Y", "green", size / 10);
-    yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
-    var axisZ = BABYLON.Mesh.CreateLines("axisZ", [
-        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
-        new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
-    ], scene);
-    axisZ.color = new BABYLON.Color3(0, 0, 1);
-    var zChar = makeTextPlane("Z", "blue", size / 10);
-    zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
-};
-
-//Local Axes
-function localAxes(size) {
-    var pilot_local_axisX = BABYLON.Mesh.CreateLines("pilot_local_axisX", [
-        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
-        new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
-    ], scene);
-    pilot_local_axisX.color = new BABYLON.Color3(1, 0, 0);
-
-    pilot_local_axisY = BABYLON.Mesh.CreateLines("pilot_local_axisY", [
-        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
-        new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
-    ], scene);
-    pilot_local_axisY.color = new BABYLON.Color3(0, 1, 0);
-
-    var pilot_local_axisZ = BABYLON.Mesh.CreateLines("pilot_local_axisZ", [
-        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
-        new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
-    ], scene);
-    pilot_local_axisZ.color = new BABYLON.Color3(0, 0, 1);
-
-    var local_origin = BABYLON.MeshBuilder.CreateBox("local_origin", { size: 1 }, scene);
-    local_origin.isVisible = false;
-
-    pilot_local_axisX.parent = local_origin;
-    pilot_local_axisY.parent = local_origin;
-    pilot_local_axisZ.parent = local_origin;
-
-    return local_origin;
-
-        }
-    {
-showAxis(4);
 }
-}
-*/
-
-
-/*
-const ground = buildGround();
-    
-const house = buildHouse();
-
-const house2 = buildHouse();
-house2.position.x = 3;
-house2.position.z = 1;
-
-const house3 = buildHouse();
-house2.position.x = 4;
-house2.position.z = 2;
-
-//Debugging
-const axis = showAxis();
-//return scene;
-*/
