26fe2ee4b60176854d9091fa082fe566a1fd3a8c
andy goldie <b00245410@studentmail.uws.ac.uk>
2023-06-05 14:41:50 +0100

    1442

--- public/JS/createScene3.js
+++ public/JS/createScene3.js
@@ -1,226 +1,65 @@
-var ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, scene);
+var createScene = function () {
 
-var waterMaterial = new BABYLON.WaterMaterial("water_material", scene);
-waterMaterial.bumpTexture = new BABYLON.Texture("bump.png", scene); // Set the bump texture
+    // Low Poly Character with Blender Tutorial of Grant Abbitt: https://www.youtube.com/user/mediagabbitt
+    // Character animations by Mixamo: https://www.mixamo.com/
 
-ground.material = waterMaterial;
+    engine.enableOfflineSupport = false;
 
-waterMaterial.addToRenderList(skybox);
-waterMaterial.addToRenderList(mesh1);
-waterMaterial.addToRenderList(mesh2);
+    // Scene and Camera
+    var scene = new BABYLON.Scene(engine);
 
-waterMaterial.windForce = 45; // Represents the wind force applied on the water surface
-waterMaterial.waveHeight = 1.3; // Represents the height of the waves
-waterMaterial.bumpHeight = 0.3; // According to the bump map, represents the pertubation of reflection and refraction
-waterMaterial.windDirection = new BABYLON.Vector2(1.0, 1.0); // The wind direction on the water surface (on width and height)
-waterMaterial.waterColor = new BABYLON.Color3(0.1, 0.1, 0.6); // Represents the water color mixed with the reflected and refracted world
-waterMaterial.colorBlendFactor = 2.0; // Factor to determine how the water color is blended with the reflected and refracted world
-waterMaterial.waveLength = 0.1; // The lenght of waves. With smaller values, more waves are generated
+    var camera1 = new BABYLON.ArcRotateCamera("camera1", Math.PI / 2, Math.PI / 4, 10, new BABYLON.Vector3(0, -5, 0), scene);
+    scene.activeCamera = camera1;
+    scene.activeCamera.attachControl(canvas, true);
+    camera1.lowerRadiusLimit = 2;
+    camera1.upperRadiusLimit = 10;
+    camera1.wheelDeltaPercentage = 0.01;
 
+    // Lights
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = 0.6;
+    light.specular = BABYLON.Color3.Black();
 
+    var light2 = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, -1.0), scene);
+    light2.position = new BABYLON.Vector3(0, 5, 5);
 
+    // Skybox
+    var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox2", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
 
-
-function createBox(scene){
-    let box = BABYLON.MeshBuilder.CreateBox("box", scene);
-    box.position.y = 3;
-    box.position.x = 1.5;
-    return box;
-}
-    
-function createLight(scene){
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0),scene);
-    light.intensity = 0.7;
-    return light;
-}
-   
-function createSphere(scene){
-    let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-    sphere.position.y = 1;
-    return sphere;
-}
-   
-function createGround(scene){
-    let ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-    return ground;
-}
-
-function createArcRotateCamera(scene){
-    let camAlpha = -Math.PI / 2,
-    camBeta  =  Math.PI / 2.5,
-    camDist  =  10,
-    camTarget = new BABYLON.Vector3(0, 0, 0); 
-    let camera = new BABYLON.ArcRotateCamera("camera1", camAlpha, camBeta, camDist, camTarget, scene);
-    camera.attachControl(true);
-    return camera;
-}
-
-export default function createStartScene(engine) {
-    let that = {};
-    let scene = that.scene = new BABYLON.Scene(engine);
-    //scene.debugLayer.show();
-
-    let box = that.starbox = createBox(scene);
-    let light = that.light = createLight(scene);
-    let sphere = that.sphere = createSphere(scene);
-    let ground = that.ground = createGround(scene);
-    let camera = that.camera = createArcRotateCamera(scene);
-    return that;
-}
-var ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, scene);
-
-var waterMaterial = new BABYLON.WaterMaterial("water_material", scene);
-waterMaterial.bumpTexture = new BABYLON.Texture("bump.png", scene); // Set the bump texture
-
-ground.material = waterMaterial;
-
-waterMaterial.addToRenderList(skybox);
-waterMaterial.addToRenderList(mesh1);
-waterMaterial.addToRenderList(mesh2);
-
-waterMaterial.windForce = 45; // Represents the wind force applied on the water surface
-waterMaterial.waveHeight = 1.3; // Represents the height of the waves
-waterMaterial.bumpHeight = 0.3; // According to the bump map, represents the pertubation of reflection and refraction
-waterMaterial.windDirection = new BABYLON.Vector2(1.0, 1.0); // The wind direction on the water surface (on width and height)
-waterMaterial.waterColor = new BABYLON.Color3(0.1, 0.1, 0.6); // Represents the water color mixed with the reflected and refracted world
-waterMaterial.colorBlendFactor = 2.0; // Factor to determine how the water color is blended with the reflected and refracted world
-waterMaterial.waveLength = 0.1; // The lenght of waves. With smaller values, more waves are generated
+    // Ground
+    var ground = BABYLON.MeshBuilder.CreateGround("ground", { height: 50, width: 50, subdivisions: 4 }, scene);
+    var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("textures/wood.jpg", scene);
+    groundMaterial.diffuseTexture.uScale = 30;
+    groundMaterial.diffuseTexture.vScale = 30;
+    groundMaterial.specularColor = new BABYLON.Color3(.1, .1, .1);
+    ground.material = groundMaterial;
 
 
+    // Load hero character and play animation
+    BABYLON.SceneLoader.ImportMesh("", "https://assets.babylonjs.com/meshes/", "HVGirl.glb", scene, function (newMeshes, particleSystems, skeletons, animationGroups) {
+        var hero = newMeshes[0];
 
+        //Scale the model down        
+        hero.scaling.scaleInPlace(0.1);
 
-function createBox(scene){
-    let box = BABYLON.MeshBuilder.CreateBox("box", scene);
-    box.position.y = 3;
-    box.position.x = 1.5;
-    return box;
-}
-    
-function createLight(scene){
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0),scene);
-    light.intensity = 0.7;
-    return light;
-}
-   
-function createSphere(scene){
-    let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-    sphere.position.y = 1;
-    return sphere;
-}
-   
-function createGround(scene){
-    let ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-    return ground;
-}
+        //Lock camera on the character 
+        camera1.target = hero;
 
-function createArcRotateCamera(scene){
-    let camAlpha = -Math.PI / 2,
-    camBeta  =  Math.PI / 2.5,
-    camDist  =  10,
-    camTarget = new BABYLON.Vector3(0, 0, 0); 
-    let camera = new BABYLON.ArcRotateCamera("camera1", camAlpha, camBeta, camDist, camTarget, scene);
-    camera.attachControl(true);
-    return camera;
-}
+        //Get the Samba animation Group
+        const sambaAnim = scene.getAnimationGroupByName("Samba");
 
-export default function createStartScene(engine) {
-    let that = {};
-    let scene = that.scene = new BABYLON.Scene(engine);
-    //scene.debugLayer.show();
+        //Play the Samba animation  
+        sambaAnim.start(true, 1.0, sambaAnim.from, sambaAnim.to, false);
 
-    let box = that.starbox = createBox(scene);
-    let light = that.light = createLight(scene);
-    let sphere = that.sphere = createSphere(scene);
-    let ground = that.ground = createGround(scene);
-    let camera = that.camera = createArcRotateCamera(scene);
-    return that;
-}const frameRate = 30;
+    });
 
-const xSlide = new BABYLON.Animation(
-    "xSlide",
-    "position.x",
-    frameRate,
-    BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-    BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
-);
-
-function createySlide(frameRate){
-    const ySlide = new BABYLON.Animation(
-        "ySlide",
-        "position.y",
-        frameRate,
-        BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-        BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
-    );
-
-    const keyFramesY = [];
-    keyFramesY.push({ frame: 0, value: 2 });
-    keyFramesY.push({ frame: frameRate, value: -2 });
-    keyFramesY.push({ frame: 2 * frameRate, value: 2 });
-
-    ySlide.setKeys(keyFramesY);
-
-    return ySlide
-}
-
-export default function createStartScene(engine) {
-    let that = {};
-    let scene = (that.scene = new BABYLON.Scene(engine));
-    //scene.debugLayer.show();
-
-    let box = (that.box = createBox(scene));
-    box.animations.push(createxSlide(frameRate));
-    box.animations.push(createySlide(frameRate));
-
-    let light = (that.light = createLight(scene));
-    let camera = (that.camera = createArcRotateCamera(scene));
-
-    that.scene.beginAnimation(box, 0, 2 * frameRate, true);
-    return that;
-}
-function createV3scaling(frameRate) {
-    const v3scaling = new BABYLON.Animation(
-        "v3Scaling",
-        "scaling",
-        frameRate,
-        BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
-        BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
-    );
-
-    const keyFramesv3s = [];
-    keyFramesv3s.push({ frame: 0, value: new BABYLON.Vector3(1, 2, 3) }),
-    keyFramesv3s.push({ frame: 0.66 * frameRate, value: new BABYLON.Vector3(2, 3, 1) });
-    keyFramesv3s.push({ frame: 1.32 * frameRate, value: new BABYLON.Vector3(3, 1, 2) });
-    keyFramesv3s.push({ frame: 2 * frameRate, value: new BABYLON.Vector3(1, 2, 3) });
-
-    v3scaling.setKeys(keyFramesv3s);
-
-    return v3scaling;
-}
-
-function createBox(scene) {
-    const box = BABYLON.MeshBuilder.CreateBox("box", {});
-    var materialBox = new BABYLON.StandardMaterial("texture1", scene);
-    box.material = materialBox;
-    box.position.x = 2;
-    return box;
-}
-function createColorShift(frameRate) {
-    const colorShift = new BABYLON.Animation(
-        "color3",
-        "material.diffuseColor",
-        frameRate,
-        BABYLON.Animation.ANIMATIONTYPE_COLOR3,
-        BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
-    );
-
-    const keyFramesC3 = [];
-    keyFramesC3.push({ frame: 0, value: new BABYLON.Color3(1, 0.5, 0.2) }),
-    keyFramesC3.push({ frame: 0.66 * frameRate, value: new BABYLON.Color3(0.5, 0.2, 1) });
-    keyFramesC3.push({ frame: 1.32 * frameRate, value: new BABYLON.Color3(0.2, 1, 0.5) });
-    keyFramesC3.push({ frame: 2 * frameRate, value: new BABYLON.Color3(1, 0.5, 0.2) });
-
-   colorShift.setKeys(keyFramesC3);
-
-    return colorShift;
+    return scene;
 }
